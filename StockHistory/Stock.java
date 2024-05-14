2133db49d0792cc350f1964e9d70089150cd532e
Daniel Nguyen <daniel.nguyen@mrjaffesclass.com>
2024-05-14 12:45:43 -0700

    Need to format

--- StockHistory/Stock.java
+++ StockHistory/Stock.java
@@ -1,5 +1,6 @@
 import com.mrjaffesclass.stockmarket.StockGetHistory;
 import com.mrjaffesclass.stockmarket.StockPrice;
+import java.util.Calendar;
 
 /**
  * Write a description of class Stock here.
@@ -12,6 +13,16 @@ public class Stock
     // instance variables - replace the example below with your own
     public static void main(String[] args) {
         StockGetHistory history = new StockGetHistory("ID62LK1OCFBCEK40", "NVDA");
-        System.out.println(history.getStockPrices());
+        for (StockPrice prices : history.getStockPrices()) {
+            double price = prices.getClose();
+            int year = prices.getDate().getTime().getYear();
+            int month = prices.getDate().getTime().getMonth();
+            int day = prices.getDate().getTime().getDay();
+            
+            
+            
+            System.out.println(year + "-" + month + "-" + day + "," + price);
+            
+        }
     }
 }
